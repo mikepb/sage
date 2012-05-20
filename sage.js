3ebe2d78ff299cb980782be6a9d24b1d35905e63
Michael Phan-Ba <michael@mikepb.com>
2012-05-19 20:07:47 -0700

    use unnamed functions

--- sage.js
+++ sage.js
@@ -11,7 +11,6 @@ Apache License
 ){
 
   var global = this;
-  var previousSage = this.sage; this.sage = sage;
 
   /**
    * Copy properties from sources to target.
@@ -22,13 +21,13 @@ Apache License
    * @api private
    */
 
-  function extend(target /* sources.. */) {
+  var extend = function(target /* sources.. */) {
     var source, key, i = 1;
     while (source = arguments[i++]) {
       for (key in source) target[key] = source[key];
     }
     return target;
-  }
+  };
 
   /**
    * Stringify value.
@@ -38,9 +37,9 @@ Apache License
    * @api private
    */
 
-  function asString(that) {
+  var asString = function(that) {
     return Object.prototype.toString.call(that);
-  }
+  };
 
   /**
    * Check if value is a string.
@@ -50,9 +49,9 @@ Apache License
    * @api private
    */
 
-  function isString(that) {
+  var isString = function(that) {
     return asString(that) == '[object String]';
-  }
+  };
 
   /**
    * Check if value is an object.
@@ -62,9 +61,9 @@ Apache License
    * @api private
    */
 
-  function isObject(that) {
+  var isObject = function(that) {
     return asString(that) == '[object Object]';
-  }
+  };
 
   /**
    * Check if value is an array.
@@ -74,9 +73,9 @@ Apache License
    * @api private
    */
 
-  function isArray(that) {
+  var isArray = function(that) {
     return asString(that) == '[object Array]';
-  }
+  };
 
   /**
    * Check if value is a function.
@@ -86,9 +85,9 @@ Apache License
    * @api private
    */
 
-  function isFunction(that) {
+  var isFunction = function(that) {
     return asString(that) == '[object Function]';
-  }
+  };
 
   /**
    * Sage library entry point.
@@ -98,9 +97,9 @@ Apache License
    *   otherwise returns a `Client`.
    */
 
-  function sage(uri) {
+  var sage = function(uri) {
     return sage.make(uri);
-  }
+  };
 
   /**
    * Restore global variable `sage` to original value.
@@ -108,6 +107,7 @@ Apache License
    * @return {sage} The `sage` library as an object.
    */
 
+  var previousSage = this.sage; this.sage = sage;
   sage.noConflict = function() {
     global.sage = previousSage;
     return sage;
