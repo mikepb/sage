51cda1b9a85d89f1d581e8578d296bbdfc13cacc
Michael Phan-Ba <michael@mikepb.com>
2013-03-12 18:50:59 -0700

    Simplify styles

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,13 @@
-// tooltips
+$(function() {
 
-$('a[rel=tooltip]').tooltip({
-  'placement': 'bottom'
-});
+  // tooltips
 
-// smooth scroll
+  $('a[rel=tooltip]').tooltip({
+    'placement': 'bottom'
+  });
 
-$(document).ready(function() {
+  // smooth scroll
+
   function filterPath(string) {
   return string
     .replace(/^\//,'')
@@ -14,81 +15,21 @@ $(document).ready(function() {
     .replace(/\/$/,'');
   }
   var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
+  var $scrollElem = $('html, body');
 
   $('a[href^=#]').each(function() {
     var thisPath = filterPath(this.pathname) || locationPath;
     if (  locationPath == thisPath
     && (location.hostname == this.hostname || !this.hostname)
     && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
-      if (target) {
-        var targetOffset = $target.offset().top;
-        $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-            location.hash = target;
-          });
+      var target = this.hash, $target;
+      if (target && ($target = $(this.hash)).length) $(this).click(function(event) {
+        event.preventDefault();
+        $scrollElem.animate({ scrollTop: $target.offset().top }, 400, function() {
+          location.hash = target;
         });
-      }
+      });
     }
   });
-
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
 
 });
-
-// subnav
-
-(function ($) {
-
-  $(function(){
-
-    // fix sub nav on scroll
-    var $win = $(window),
-        $body = $('body'),
-        $nav = $('.subnav'),
-        navHeight = $('.navbar').first().height(),
-        subnavHeight = $('.subnav').first().height(),
-        subnavTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
-        marginTop = parseInt($body.css('margin-top'), 10);
-        isFixed = 0;
-
-    processScroll();
-
-    $win.on('scroll', processScroll);
-
-    function processScroll() {
-      var i, scrollTop = $win.scrollTop();
-
-      if (scrollTop >= subnavTop && !isFixed) {
-        isFixed = 1;
-        $nav.addClass('subnav-fixed');
-        $body.css('margin-top', marginTop + subnavHeight + 'px');
-      } else if (scrollTop <= subnavTop && isFixed) {
-        isFixed = 0;
-        $nav.removeClass('subnav-fixed');
-        $body.css('margin-top', marginTop + 'px');
-      }
-    }
-
-  });
-
-})(window.jQuery);
